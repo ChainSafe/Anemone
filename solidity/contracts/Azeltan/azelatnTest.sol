fa506999772d7441a6c6f14ac8f5313b58fd5128
steviezhang <stephanie@chainsafe.io>
2020-01-13 13:02:16 -0500

    stubbed out tests for azeltan fork

---
+++ solidity/contracts/Azeltan/azelatnTest.sol
@@ -0,0 +1,20 @@
+// things to test in this contract
+
+contract AzeltanTest {
+
+    // EIP-152: Add Blake2 compression function F precompile
+    function testEIP152 {}
+
+    // EIP-1108: Reduce alt_bn128 precompile gas costs
+    function testEIP1108{}
+
+    // EIP-1344: Add ChainID opcode
+    function testEIP1344{}
+
+    // EIP-2028: Calldata gas cost reduction
+    function testEIP2029{}
+
+    // EIP-2200: Rebalance net-metered SSTORE gas cost with consideration of SLOAD gas cost change
+    function testEIP2200{}
+
+}
