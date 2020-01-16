307bc4483d49a4e896a8bc4ab61f68687cbfd8c9
steviezhang <stephanie@chainsafe.io>
2020-01-16 15:17:06 -0500

    added blake precompile test

--- solidity/contracts/Azeltan/azelatnTest.sol
+++ solidity/contracts/Azeltan/azelatnTest.sol
@@ -3,14 +3,41 @@
 contract AzeltanTest {
 
     // EIP-152: Add Blake2 compression function F precompile
-    function testEIP152 {}
+    function testEIP152() {
+
+        uint32 rounds = 12;
+
+        bytes32[2] memory h;
+        h[0] = hex"48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5";
+        h[1] = hex"d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b";
+
+        bytes32[4] memory m;
+        m[0] = hex"6162630000000000000000000000000000000000000000000000000000000000";
+        m[1] = hex"0000000000000000000000000000000000000000000000000000000000000000";
+        m[2] = hex"0000000000000000000000000000000000000000000000000000000000000000";
+        m[3] = hex"0000000000000000000000000000000000000000000000000000000000000000";
+
+        bytes8[2] memory t;
+        t[0] = hex"03000000";
+        t[1] = hex"00000000";
+
+        bool f = true;
+
+        bytes memory args = abi.encodePacked(rounds, h[0], h[1], m[0], m[1], m[2], m[3], t[0], t[1], f);
+
+        assembly {
+            staticcall(not(0), 0x09, add(args, 32), 0xd5, output, 0x40)
+        }
+    }
 
     // EIP-1108: Reduce alt_bn128 precompile gas costs
     // Contract	        Address	Current Gas Cost	    Updated Gas Cost
     // ECADD	        0x06	500 	                150
     // ECMUL	        0x07	40 000          	    6 000
     // Pairing check	0x08	80 000 * k + 100 000    34 000 * k + 45 000
-    function testEIP1108{
+    function testEIP1108() {
+
+        //TODO find valid keys :'(
 
         // call ECADD
         uint256[3] memory input1;
@@ -44,7 +71,7 @@ contract AzeltanTest {
     }
 
     // EIP-1344: Add ChainID opcode
-    function testEIP1344{
+    function testEIP1344() {
         chainid()
 
 
@@ -53,7 +80,7 @@ contract AzeltanTest {
     // EIP-2028: Calldata gas cost reduction
     // From 68 gas per byte, to 16 gas per byte
     // so calldataload should cost 16 (gas) x 32 (bytes) or 512 gas now
-    function testEIP2029{
+    function testEIP2029() {
         assembly {
             let a: = mload(0x40)
             let b: = add(a, 32)
@@ -85,7 +112,7 @@ contract AzeltanTest {
     //                 If original value is 0, add SSTORE_SET_GAS - SLOAD_GAS to refund counter.
     //                 Otherwise, add SSTORE_RESET_GAS - SLOAD_GAS gas to refund counter.
 
-    function testEIP2200{
+    function testEIP2200 () {
         uint256 startgas
         uint256 endgas
 
