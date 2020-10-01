0b07a9499a20f1acafe030b940e5cf6c3831f4e1
Gregory Markou <gregorymarkou@gmail.com>
2020-09-30 23:29:31 -0400

    attemptoveride

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,22 @@ program.description("Anemone is an Ethereum node compatibility testing tool.")
 // Comands
 import * as commands from "./cmds"; 
 
+program.exitOverride();
 program.allowUnknownOption(false);
 
 for (let cmd in commands) {
     program.addCommand(commands[cmd])
 }
 
-(async () => {
-    if (process.argv && process.argv.length <= 2) {
+if (process.argv && process.argv.length <= 2) {
+    program.help();
+} else {
+    try {
+        program.parse(process.argv);
+    } catch (e) {
         program.help();
-    } else {
-        try {
-            await program.parseAsync(process.argv);
-        } catch (e) {
-            console.log({ e });
-            process.exit(1)
-        }
+        console.log({ e });
+        process.exit(1)
     }
-})()
+}
 
